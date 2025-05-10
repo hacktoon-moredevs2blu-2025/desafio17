85b71000b8a2950b750829603d74ada4b469fc65
JoaoEmanoelConinch <jecconinch@gmail.com>
2025-05-09 23:23:30 -0300

    Update Ansible hosts configuration, add .gitattributes and .gitignore, and create startup script for infrastructure setup

--- terraform/infra/main.tf
+++ terraform/infra/main.tf
@@ -113,6 +113,13 @@ resource "aws_key_pair" "lb_ssh_key_pair" {
   public_key = tls_private_key.lb_ssh_key.public_key_openssh
 }
 
+resource "local_file" "ssh_key_file" {
+  content         = tls_private_key.lb_ssh_key.private_key_pem
+  filename        = "./key_pair.pem"
+  file_permission = "0600"
+}
+
+
 # Elastic IP fixo para a EC2
 resource "aws_eip" "app_eip" {
   instance = aws_instance.app_server.id
