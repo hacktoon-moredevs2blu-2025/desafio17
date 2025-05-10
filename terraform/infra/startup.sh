7bc69dc6f4fb16530a6d019a4b87460ef408579d
JoaoEmanoelConinch <jecconinch@gmail.com>
2025-05-09 21:08:11 -0300

    Remove outdated startup script for infrastructure setup

--- terraform/infra/startup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo apt update -y
-sudo apt install -y nginx openjdk-17-jdk unzip
-# Supondo que Angular esteja buildado e copiado para /var/www/html
-# Supondo que o JAR esteja em /home/ubuntu/app/app.jar
-nohup java -jar /home/ubuntu/app/app.jar > /home/ubuntu/app.log 2>&1 &
